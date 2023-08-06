70a076b6a0e948b8dd7273c32a7c99b9595cb31e
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-08-06 17:59:23 +0530

    using pickle to save and retrieve model

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 import sklearn.model_selection
 from sklearn import linear_model
 import pandas as pd
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 print("Hello World!")
 data = pd.read_csv('student-mat.csv', sep=';')
@@ -17,11 +20,18 @@ X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y
 linear = linear_model.LinearRegression()
 linear.fit(X_train, y_train)
 acc = linear.score(X_test, y_test)
+print(acc)
 
+with open('studentmodel.pickle', 'wb') as file:
+    pickle.dump(linear, file)
+
+file = open('studentmodel.pickle', 'rb')
+
+linear = pickle.load(file)
 print('Coefficients: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
 
-predictions  = linear.predict(X_test)
+predictions = linear.predict(X_test)
 
 for i in range(len(predictions)):
     print(predictions[i], X_test[i], y_test[i])
